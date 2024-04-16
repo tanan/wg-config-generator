47e64f059893c6ac917e01da6f2dc5ed2e9b6614
tanan <toshifumi.anan@gmail.com>
2024-04-16 09:59:58 +0900

    add test

---
+++ config/config_test.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestLoadConfig(t *testing.T) {
+	cfg := Config{
+		WorkDir: "/etc/wireguard",
+		Server: ServerConfig{
+			Endpoint: "10.0.0.1:51820",
+			Port:     51820,
+			WireguardInterface: WireguardInterface{
+				Name:    "wg0",
+				Address: "10.0.0.1",
+				Network: "10.0.0.1/24",
+			},
+		},
+	}
+	tests := []struct {
+		name       string
+		configFile string
+		want       Config
+		isEqual    bool
+	}{
+		{name: "success", configFile: "./testdata/config.match.yaml", want: cfg, isEqual: true},
+		{name: "error", configFile: "./testdata/config.unmatch.yaml", want: cfg, isEqual: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			LoadConfig(tt.configFile)
+			got := GetConfig()
+			if reflect.DeepEqual(got, tt.want) != tt.isEqual {
+				t.Errorf("LoadConfig() = %v, want %v, isEqual %v", got, tt.want, tt.isEqual)
+			}
+		})
+	}
+}
